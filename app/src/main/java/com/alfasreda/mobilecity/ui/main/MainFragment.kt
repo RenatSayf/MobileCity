5a74e20836497d5df5cecbc24275740db004428f
renatsayf <renatsayf@gmail.com>
2022-03-03 21:53:59 +0500

    MainFragment.kt запилил связь с маяком

--- app/src/main/java/com/alfasreda/mobilecity/ui/main/MainFragment.kt
+++ app/src/main/java/com/alfasreda/mobilecity/ui/main/MainFragment.kt
@@ -1,12 +1,13 @@
+@file:Suppress("ObjectLiteralToLambda")
+
 package com.alfasreda.mobilecity.ui.main
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothManager
-import android.bluetooth.le.*
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertiseSettings
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -19,11 +20,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.alfasreda.mobilecity.R
 import com.alfasreda.mobilecity.databinding.MainFragmentBinding
-import com.alfasreda.mobilecity.services.BluetoothService
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import quevedo.soares.leandro.androideasyble.BLE
+import com.fondesa.kpermissions.allGranted
+import com.fondesa.kpermissions.extension.permissionsBuilder
+import com.fondesa.kpermissions.extension.send
 
 
 class MainFragment : Fragment(R.layout.main_fragment) {
@@ -31,8 +30,6 @@ class MainFragment : Fragment(R.layout.main_fragment) {
     private lateinit var binding: MainFragmentBinding
 
     private val mainVM: MainViewModel by viewModels()
-
-    private var ble: BLE? = null
 
     private var bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
     private val bltAdvertiser = bluetoothAdapter?.bluetoothLeAdvertiser
@@ -44,20 +41,10 @@ class MainFragment : Fragment(R.layout.main_fragment) {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    //@SuppressLint("MissingPermission")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = MainFragmentBinding.bind(view)
-
-        if (bluetoothAdapter == null) {
-            Toast.makeText(requireContext(), "Устройство не поддерживает блютуз", Toast.LENGTH_LONG).show()
-            return
-        }
-        else if (bluetoothAdapter != null && !bluetoothAdapter!!.isEnabled) {
-            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBtIntent, 1111)
-        }
 
         val sid = ("ID00000004").substring(2)
         val namesSnd = sid.toByte()
@@ -74,9 +61,7 @@ class MainFragment : Fragment(R.layout.main_fragment) {
             override fun onStartSuccess(settingsInEffect: AdvertiseSettings?) {
                 super.onStartSuccess(settingsInEffect)
                 binding.progressAvailableDevices.visibility = View.GONE
-                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED) {
-                    return
-                }
+                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED) return
                 bluetoothAdapter?.startLeScan(object : BluetoothAdapter.LeScanCallback {
                     override fun onLeScan(p0: BluetoothDevice?, p1: Int, p2: ByteArray?) {
                         p0
@@ -85,21 +70,21 @@ class MainFragment : Fragment(R.layout.main_fragment) {
                     }
                 })
             }
-
             override fun onStartFailure(errorCode: Int) {
                 super.onStartFailure(errorCode)
                 binding.progressAvailableDevices.visibility = View.GONE
             }
         }
 
-        val checkSelfPermission = ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_SCAN)
-        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED) {
+        permissionsBuilder(Manifest.permission.ACCESS_FINE_LOCATION).build().send(){ result ->
+            if (result.allGranted()) {
+                initBluetooth(bluetoothAdapter)
+            }
+        }
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             bluetoothAdapter?.stopLeScan(object : BluetoothAdapter.LeScanCallback {
-                override fun onLeScan(p0: BluetoothDevice?, p1: Int, p2: ByteArray?) {
-                    p0
-                    p1
-                    p2
-                }
+                override fun onLeScan(p0: BluetoothDevice?, p1: Int, p2: ByteArray?) {}
             })
         }
 
@@ -117,12 +102,20 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1111 && resultCode != Activity.RESULT_OK) return
+    private fun initBluetooth(adapter: BluetoothAdapter?) {
+        if (adapter == null) {
+            Toast.makeText(requireContext(), "Устройство не поддерживает блютуз", Toast.LENGTH_LONG).show()
+            return
+        }
+        else if (!adapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, 1111)
+        }
     }
 
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 
 
 
